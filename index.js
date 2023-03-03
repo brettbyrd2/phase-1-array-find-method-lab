0b748374929e336070a5b0306ed2bf1d579c2653
Brett Byrd <byrdb2@yahoo.com>
2023-03-03 17:05:05 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // code your solution here
+
+function superbowlWin(record){ 
+  const found = record.find(object=> object.result === 'W')
+
+  if(found === undefined){
+  return undefined
+  }
+  else{
+  return found.year
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function superbowlWin(record){
+
+  
+
+//  const found = record.find(object=>object.result === 'W')
+ 
+//   if(found === undefined){
+//     return undefined
+//   }
+//   else{
+//     return found.year
+//   }
+
+
+
+// }
+
+  
+  
+    
